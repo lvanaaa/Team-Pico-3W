60976548921803ad52bdbc9576add649064dfb5a
Luana-Maria Militaru <164344821+lvanaaa@users.noreply.github.com>
2025-03-22 12:29:04 +0200

    Update hi.rs

--- src/bin/hi.rs
+++ src/bin/hi.rs
@@ -1,25 +1,37 @@
-// +---------------------------------------------------------------------------+
-// |                            Embassy March 2025                             |
-// |                            Hi, I am new here!                             |
-// +---------------------------------------------------------------------------+
-
 #![no_std]
 #![no_main]
 
+use core::cmp::min;
+
+use cortex_m_rt::entry;
+use defmt::{info, panic};
+use defmt_rtt as _;
 use embassy_executor::Spawner;
-// Use the `panic_probe` crate to provided the panic handler and the 
-// `defmt_rtt` to import the runtime for defmt, to be able to use
-// the print macros.
-use {defmt_rtt as _, panic_probe as _};
-// Use the logging macros provided by defmt.
-#[allow(unused)]
-use defmt::*;
+use embassy_rp::adc::InterruptHandler;
+use embassy_rp::config::Config;
+use embassy_rp::gpio::{Input, Pull};
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::pwm::SetDutyCycle;
+use embassy_time::{Duration, Timer};
+use fixed::traits::ToFixed;
+use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // Get a handle to the RP's peripherals.
-    let _p = embassy_rp::init(Default::default());
+    let p = embassy_rp::init(Default::default());
 
+    let mut button1 = Input::new(p.PIN_2, Pull::Up);
+    let mut button2 = Input::new(p.PIN_3, Pull::Up);
+    let mut button3 = Input::new(p.PIN_4, Pull::Up);
+    let mut button4 = Input::new(p.PIN_6, Pull::Up);
     loop {
+        button1.wait_for_falling_edge().await;
+        info!("Team name: Pico 3W");
+        button2.wait_for_falling_edge().await;
+        info!("Team name: Pico 3W");
+        button3.wait_for_falling_edge().await;
+        info!("Team name: Pico 3W");
+        button4.wait_for_falling_edge().await;
+        info!("Team name: Pico 3W");
     }
 }
